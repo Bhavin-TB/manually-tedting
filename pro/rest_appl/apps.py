84c2f2346a20881bf5737168cf6c7591a865e10d
Bhavin <bhavin.tb@uvionicstech.com>
2020-08-28 11:07:21 +0530

    fir pro

---
+++ pro/rest_appl/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RestApplConfig(AppConfig):
+    name = 'rest_appl'
---
+++ pro/rest_appl/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+]
